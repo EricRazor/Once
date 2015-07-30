ed9cc9671377da61458e66efad8e67ca42a45fbd
EricRazor <eric.razorjr@gmail.com>
2015-07-29 22:30:34 -0700

    Initial Commit

---
+++ Official once/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Official once
+//
+//  Created by Eric  on 7/29/15.
+//  Copyright (c) 2015 Eric Razor Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
